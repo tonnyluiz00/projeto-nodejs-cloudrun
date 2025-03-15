c35e84d2bea8ccca3aa796be291ac1f6ceeae5dc
TonnyLuiz00 <tonnyluiz00@gmail.com>
2025-03-15 14:04:55 -0300

    Commit Nova mensagem

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('JEFERSON GOSTOSAO');
+  res.send('Ola Mundo! Feito por Tonny Luiz');
 });
 
 app.listen(PORT, HOST, () => {
